68b03464ecf19c898f22eec5fe494c161de832c9
Nazia Khan <nk9555100@gmail.com>
2026-03-24 17:05:33 +0530

    Ignore target directory

--- src/test/java/PageObjects/cartPage.java
+++ src/test/java/PageObjects/cartPage.java
@@ -10,9 +10,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class cartPage extends basePage {
+public class CartPage extends basePage {
 
-	public cartPage(WebDriver driver) {
+	public CartPage(WebDriver driver) {
 		super(driver);
 		// TODO Auto-generated constructor stub
 		
